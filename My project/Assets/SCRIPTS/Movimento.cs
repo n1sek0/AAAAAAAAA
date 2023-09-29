8d054c3ef8f255b3b2fb0a58e1d554163fee1a60
ISRAEL DAVID DA COSTA SILVA <20211170150022@ifrn.local>
2023-09-29 10:59:51 -0300

    fiz ele pular e terminei a anim
    aaaaaaaaaa

--- My project/Assets/SCRIPTS/Movimento.cs
+++ My project/Assets/SCRIPTS/Movimento.cs
@@ -7,9 +7,13 @@ public class Movimento : MonoBehaviour
     private CharacterController character;
     private Animator animator;
     private Vector3 inputs;
+    private float directionY;
+    [SerializeField] private float gravityForce = 8;
+    [SerializeField] private float jumpForce = 8;
+    private bool isJumping;
 
     private float velocidade = 3f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +24,43 @@ public class Movimento : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        Vector3 movement = Vector3.zero;
+
+        if (!character.isGrounded)
+        {
+            directionY -= gravityForce * Time.deltaTime;
+        }
+        
+        if (character.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            directionY = jumpForce;
+            animator.SetBool("jumping", true);
+            isJumping = true;
+        } else if (character.isGrounded)
+        {
+            isJumping = false;
+            animator.SetBool("jumping", false);
+        }
+
+        movement.y = directionY;
+        movement *= Time.deltaTime;
+        character.Move(movement);
+        
         inputs.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         character.Move(inputs * Time.deltaTime * velocidade);
+        Debug.Log(character.isGrounded);
+        
+        if (inputs != Vector3.zero)
+        {
+            animator.SetBool("running", true);
+            transform.forward = inputs;
+        }
+        else
+        {
+            animator.SetBool("running", false);
+        }
+
+        //isJumping = false;
     }
 }
