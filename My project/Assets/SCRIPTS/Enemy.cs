92f56abc2128c152244776a157fc234e68144c53
ISRAEL DAVID DA COSTA SILVA <20211170150022@ifrn.local>
2023-10-24 09:29:14 -0300

    awseqweqew
    qweqweqweqwe

--- My project/Assets/SCRIPTS/Enemy.cs
+++ My project/Assets/SCRIPTS/Enemy.cs
@@ -1,7 +1,8 @@
+using System;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
-
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
@@ -10,16 +11,16 @@ public class Enemy : MonoBehaviour
     public NavMeshAgent agent;
     public Transform player;
     public LayerMask whatIsGround, whatIsPlayer;
-    
+
     //Patrolling
     public Vector3 walkPoint;
     private bool walkPointSet;
     public float walkPointRange;
-    
+
     //Attacking
     public float timeBetweenAttacks;
     private bool alreadyAttacked;
-    
+
     //States
     public float sightRange, attackRange;
     public bool playerInSightRange, playerInAttackRange;
@@ -34,15 +35,15 @@ public class Enemy : MonoBehaviour
     {
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
-        
-        if(!playerInSightRange && !playerInAttackRange) Patrolling();
-        if(playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if(playerInAttackRange && playerInSightRange) AttackPlayer();
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
     }
 
     void Patrolling()
     {
-        if (walkPointSet) SearchWalkPoint();
+        if (!walkPointSet) SearchWalkPoint();
 
         if (walkPointSet)
         {
@@ -50,6 +51,12 @@ public class Enemy : MonoBehaviour
         }
 
         Vector3 distanceToWalkPoint = transform.position - walkPoint;
+        //Walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+
     }
 
     void SearchWalkPoint()
@@ -67,14 +74,27 @@ public class Enemy : MonoBehaviour
 
     void ChasePlayer()
     {
-        
+        agent.SetDestination(player.position);
     }
 
     void AttackPlayer()
     {
-        
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
     }
-    
+
+    void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
     void Start()
     {
         currentHealth = maxHealth;
@@ -83,7 +103,7 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
-        
+
         // Check if the enemy's health reaches zero
         if (currentHealth <= 0)
         {
@@ -94,5 +114,14 @@ public class Enemy : MonoBehaviour
     private void Die()
     {
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+
     }
 }
