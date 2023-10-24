51f26cecbfe39a1b2b374b3094dc384b91969222
ISRAEL DAVID DA COSTA SILVA <20211170150022@ifrn.local>
2023-10-24 08:19:03 -0300

    asdasdasdasdasd

--- My project/Assets/SCRIPTS/Enemy.cs
+++ My project/Assets/SCRIPTS/Enemy.cs
@@ -1,10 +1,80 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 100f;
     private float currentHealth;
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+    
+    //Patrolling
+    public Vector3 walkPoint;
+    private bool walkPointSet;
+    public float walkPointRange;
+    
+    //Attacking
+    public float timeBetweenAttacks;
+    private bool alreadyAttacked;
+    
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
 
+    void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        
+        if(!playerInSightRange && !playerInAttackRange) Patrolling();
+        if(playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if(playerInAttackRange && playerInSightRange) AttackPlayer();
+    }
+
+    void Patrolling()
+    {
+        if (walkPointSet) SearchWalkPoint();
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+    }
+
+    void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    void ChasePlayer()
+    {
+        
+    }
+
+    void AttackPlayer()
+    {
+        
+    }
+    
     void Start()
     {
         currentHealth = maxHealth;
